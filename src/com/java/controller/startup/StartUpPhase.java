d45e35eb48db161640120e1beddf2ccecdd614c7
Cristian Rodriguez <13244760+Cristianbcd@users.noreply.github.com>
2019-03-02 15:49:39 -0500

    Added Coments and some refactor.

--- src/com/java/controller/startup/StartUpPhase.java
+++ src/com/java/controller/startup/StartUpPhase.java
@@ -1,3 +1,7 @@
+/**
+ * SOEN 6441 Winter 2019
+ * Risk Game
+ */
 package com.java.controller.startup;
 
 import java.util.*;
@@ -9,63 +13,92 @@ import com.java.model.map.Country;
 import com.java.model.map.GameMap;
 import com.java.model.player.Player;
 
+/**
+ * This class includes the logic to set up the game before it starts.
+ * The methods generate the players in a round robin fashion, based on the highest number from the dice. 
+ * The countries are given randomly to each player.
+ * The initial army calculation is based on the number of players.
+ * We provide a sort method to resolve ties between players to generate the round robin order.
+ * 
+ * @author Arnav Bhardwaj
+ * @author Karan Dhingra
+ * @author Ghalia Elkerdi
+ * @author Sahil Singh Sodhi
+ * @author Cristian Rodriguez 
+ * @version 1.0.0
+ */
 public class StartUpPhase {
-
+	
+	/**
+	 * Game Data holds the whole data of the game, here the static variable holds the ids for the players.
+	 * In that way it is possible to identify players even if the have the same name.
+	 */
 	public GameData gameData;
 	private static int playerId = 1;
 	
+	/**
+	 * Default constructor receives game data as a parameter. Because this is the set up phase class every data
+	 * is going to be calculated and set.
+	 * 
+	 * @param gameData the game data object be set up.
+	 */
 	public StartUpPhase(GameData gameData) {
 		this.gameData = gameData;
 	}
 	
-	public GameData start() {
-		return null;
-	}
-	
-	public Integer getNoOfPlayerFromUser() {
-		return null;
-	}
-	
-	public ArrayList<String> getPlayersNamesFromUser() {
-		return null;
-	}
-	
+	/**
+	 * This methods gives unique ids to the players by receiving a collection of player names. Here the static
+	 * the static variable is incremented by one every time a new player is created.
+	 * 
+	 * @param playerNames the names of the players given by the players through the console
+	 */
 	public ArrayList<Player> generatePlayers(ArrayList<String> playerNames){
-
+		
+		// A collection of empty players is created.
 		ArrayList<Player> newPlayers = new ArrayList<Player>();
-
+		
+		// Give every new player a unique id and the name provided by the user.
 		for(int i=0; i < playerNames.size(); i++){
 			newPlayers.add(new Player(playerId,playerNames.get(i)));
 			playerId++;
 		}
 		
-		// assign to game data the players
+		// Set the player collection with the new one.
 		gameData.setPlayers(newPlayers);
 		
 		return newPlayers;
 	}
 	
+	/**
+	 * The approach to give the countries to the players is random. That means that each contry is randomly
+	 * given to any player. But each player receives the same number of countries.
+	 * 
+	 */
 	public void assignCountriesToPlayers() {
 		
+		// Get the list of players from game data.
 		ArrayList<Player> players = gameData.getPlayers();
 
 		// Used to obtain the country objects
 		HashMap<String, Country> countryObject = gameData.gameMap.getAllCountries();
 
-		// start randomly assigning the countries evenly to the players
+		// Start randomly assigning the countries evenly to the players
 		String[] countiresToAssign = Arrays.copyOf(countryObject.keySet().toArray(), countryObject.size(),String[].class);
 
-		// converting to arraylist for shuffling the elements (countries)
+		// Converting to arraylist for shuffling the elements (countries)
 		ArrayList<String> countiresToAssignArrayList =  new ArrayList<String>(Arrays.asList(countiresToAssign));
-
+		
+		// The order of the countries are randomly shuffle in order to give them one by one.
 		Collections.shuffle(countiresToAssignArrayList);
-
+		
+		// Helper variables to assign the countries.
 		int numOfCountriesToAssign = countiresToAssignArrayList.size();
 		int numberOfPlayers = gameData.getNoOfPlayers();
-		// this index keeps track of the current index of array countries
+		
+		// This index keeps track of the current index of array countries
 		int indexKeeper = 0;
 		
-		// iterating the players to assign them the countries
+		// Iterating the players to assign them the countries in a fair way.
 		for(int i = 0; i < numberOfPlayers; i++) {
 			int numberOfCountriesToBeAssignedToPlayer = (numOfCountriesToAssign - indexKeeper) / (numberOfPlayers - i);
 			for(int j = indexKeeper; j < indexKeeper + numberOfCountriesToBeAssignedToPlayer; j++) {
@@ -76,6 +109,12 @@ public class StartUpPhase {
 		
 	}
 	
+	/**
+	 * The initial army calculation is given by the number of players.
+	 * 
+	 * @param numPlayers based on the number of players the method returns the initial army per player.
+	 * 
+	 */
 	public Integer initialArmyCalculation(Integer numPlayers) {
 		Integer numberOfArmiesPerPlayer = new Integer(0);
 		switch(numPlayers) {
@@ -97,35 +136,44 @@ public class StartUpPhase {
 		}
 		return numberOfArmiesPerPlayer;
 	}
-
-	public CardsDeck generateCardsDeck() {
-		return null;
-	}
-
+	
+	/**
+	 * The order of play is given my the max result of the dices after every player roll a dice. If there is
+	 * a tie the player roll again until one has a greter result.
+	 * 
+	 */
 	public ArrayList<Player> generateRoundRobin(){
 
-		//iterate over players and set the order of play
+		// Auxiliary variables to perform the order of player calculation.
 		ArrayList<Player> temp = new ArrayList<Player>();
 		ArrayList<Player> tiePlayers = gameData.getPlayers();
-		ArrayList<String> results = new ArrayList<String>();
+		Boolean flag = false;
 		
+		// Dice object to get the random numbers from 1 to 6
 		Dice dice = new Dice();
 		int aux = tiePlayers.size();
 
-		// this will resolve ties based on the die rolled
+		// The following loop will resolve ties based on the die rolled.
 		while(!tiePlayers.isEmpty()) {
-			results.add(" ");
 
-			// assign the player object to the roll die integer amount
+			// Assign the player object to the roll die integer amount
 			for(int k = 0; k < aux; k++){
-				tiePlayers.get(k).setOrderOfPlay(dice.rollDice());
-				results.add("Player " + tiePlayers.get(k).getPlayerName() + " rolled the dice: " + tiePlayers.get(k).getOrderOfPlay());
+				try {
+					tiePlayers.get(k).setOrderOfPlay(dice.rollDice());
+				} catch(Exception e) {
+					// If something goes wrong with the return.
+					for(Player tie : tiePlayers) {
+						temp.add(tie);
+					}
+					break;
+				}
+				//System.out.println("Player " + tiePlayers.get(k).getPlayerName() + " rolled the dice: " + tiePlayers.get(k).getOrderOfPlay());
 			}
 
-            // when the player roll dice the tie has to be resolved by calling custom sort from 0 till
+            // When the player roll the dice the tie has to be resolved by calling custom sort from 0 till the index of tie player.
 			tiePlayers = sort(tiePlayers, 0, aux);
 
-            // to check if the first two players do not have the same number rolled add to temp array
+            // To check if the first two players do not have the same number rolled add to temp array
 			if(tiePlayers.size() > 1 && tiePlayers.get(0).getOrderOfPlay() != tiePlayers.get(1).getOrderOfPlay()) {
 				temp.add(tiePlayers.get(0));
 				tiePlayers.remove(0);
@@ -135,34 +183,53 @@ public class StartUpPhase {
 				temp.add(tiePlayers.get(0));
 				tiePlayers.remove(0);
 			}
-
-
-			Boolean flag = false;
-
+			
+			// Gets the index of tie players.
 			for(int i = 0; i < tiePlayers.size() - 1; i++) {
-				if(tiePlayers.get(0).getOrderOfPlay() == tiePlayers.get(i+1).getOrderOfPlay()) {
-					aux = i + 1 + 1;
-					flag = true;
+				try {
+					if(tiePlayers.get(0).getOrderOfPlay() == tiePlayers.get(i+1).getOrderOfPlay()) {
+						aux = i + 1 + 1;
+						flag = true;
 
-				} else {
-					if(!flag) {
-						aux = 0;
+					} else {
+						if(!flag) {
+							aux = 0;
+						}
+						break;
+					}
+				} catch(Exception e) {
+					// If something goes wrong with the return.
+					for(Player tie : tiePlayers) {
+						temp.add(tie);
 					}
 					break;
 				}
 			}
 		}
+		
+		// Sets the order of play.
 		for(int i = 0; i < temp.size(); i++) {
 			temp.get(i).setOrderOfPlay(i+1);
 		}
+		
+		// Sets the player list in order of play.
 		gameData.setPlayers(temp);
+		
 		return gameData.getPlayers();
 	}
 
-
-	//custom sorting that allows to pick the start and stop index passed from user.
+	/**
+	 * Custom sorting that allows to sort from a given index to another index of the array
+	 * to be sorted.
+	 * 
+	 * @param array Array to be sorted.
+	 * @param from index from where to start sorting.
+	 * @param to index from where to end sorting.
+	 * 
+	 */
 	private ArrayList<Player> sort(ArrayList<Player> array, int from, int to) {
-
+		
+		// Each element of the array is compared with the rest of elements.
 		for(int i = from; i < to; i++) {
 			for(int j = from; j < to; j++) {
 				if(array.get(i).getOrderOfPlay() > array.get(j).getOrderOfPlay() ) {
@@ -174,12 +241,33 @@ public class StartUpPhase {
 		}
 		return array;
 	}
-
-	public void placeArmies() {
-		
+	
+	// TODO: Implement this method in the next built or erase it. 
+	public CardsDeck generateCardsDeck() {
+		return null;
 	}
 	
+	// TODO: Implement this method in the next built or erase it. 
+	public void placeArmies() {
+	}
+	
+	// TODO: Implement this method in the next built or erase it. 
 	public Integer getNumberOfArmiesToAllocate(Integer noOfPlayers) {
+		return null;
+	}
+	
+	// TODO: Implement this method in the next built or erase it. 
+	public GameData start() {
+		return null;
+	}
+	
+	// TODO: Implement this method in the next built or erase it. 
+	public Integer getNoOfPlayerFromUser() {
+		return null;
+	}
+	
+	// TODO: Implement this method in the next built or erase it. 
+	public ArrayList<String> getPlayersNamesFromUser() {
 		return null;
 	}
 }
--- src/com/java/view/RiskGameDriver.java
+++ src/com/java/view/RiskGameDriver.java
@@ -20,13 +20,17 @@ import java.util.Scanner;
  * Each player will be able to place their armies on the assigned country based on the army total.
  *
  * @author Arnav Bhardwaj
- * @author Cristian Rodriguez
+ * @author Karan Dhingra
+ * @author Ghalia Elkerdi
+ * @author Sahil Singh Sodhi
+ * @author Cristian Rodriguez 
  * @version 1.0.0
  */
 public class RiskGameDriver {
 
 	private GameData gameData;
 	private StartUpPhase startUp;
+	Scanner input = new Scanner(System.in);
 
     /**
      * Constructor will allow to load the map from user selection.
@@ -34,26 +38,34 @@ public class RiskGameDriver {
      */
 	public RiskGameDriver() {
 		gameData = new GameData();
-		MapLoader maploader = new MapLoader(); // using this will load the map
+		// using this will load the map
+		MapLoader maploader = new MapLoader();
 		gameData.gameMap = maploader.loadMap();
 	}
 
     /**
-     * begins the console interface by initating the startup
+     * Begins the console interface by initating the startup
      */
 	public void startGame() {
 		// Call the helpers here
 		initiateStartUpPhase();
+		initialArmyPlacement();
+		ramdomAssignationOfCountries();
+		initiateRoundRobin();
 	}
-
-
+	
+	/**
+     * The Start up phase intiates in this method by calling the helpers from StartUp phase. This method asks
+     * for the names of the players and the number of players.
+     */
 	private void initiateStartUpPhase() {
-		// need to call the controller start uphase
+		
+		// Need to call the controller start uphase
 		startUp = new StartUpPhase(gameData);
-		Scanner input = new Scanner(System.in);
 		int numOfPlayer = 0;
 		String numOfPlayerStr = "";
-
+		
+		// Retrives the number of players provided by the user.
 		do {
 
 			System.out.println("\nNote: You can only have players between 2 to " + gameData.gameMap.getNumberOfCountries());
@@ -64,14 +76,15 @@ public class RiskGameDriver {
 		} while (isNaN(numOfPlayerStr)
                 || Integer.parseInt(numOfPlayerStr) < GameData.MIN_PLAYERS
 				|| Integer.parseInt(numOfPlayerStr) > Math.min(GameData.MAX_PLAYERS, gameData.gameMap.getNumberOfCountries()));
-
+		
+		// Stores the number of players in game data.
 		numOfPlayer = Integer.parseInt(numOfPlayerStr);
-
-		// set values in the game data
 		gameData.setNoOfPlayers(numOfPlayer);
-
+		
+		// Array list to store the name of the players provided by the user.
 		ArrayList<String> playerNames = new ArrayList<String>();
-
+		
+		// Asks the name of each player.
 		for (int i = 0; i < numOfPlayer; i++) {
 			
 			String playerNameInput = "";
@@ -85,64 +98,90 @@ public class RiskGameDriver {
 			playerNames.add(playerNameInput.trim());
 		}
 		
+		// Array list to store the name of the players provided by the user.
 		startUp.generatePlayers(playerNames);
-
-		// now start the roundrobin order
-		System.out.println(" ");
-		initiateRoundRobin();
+	}
+	
+	/**
+     * Every country owned by a player start at least with one army at the beginning, then the player can choose where to put
+     * the remaining armies.
+     */
+	private void initialArmyPlacement() {
 		
-		// Country Assignment begins
-		startUp.assignCountriesToPlayers();
-
 		// Calculation of initial army
+		Integer numberOfArmiesAvailablePerPlayer;
 		System.out.println("Calculation of initial armies done....");
 
+		// Gets all countries from game map.
 		HashMap<String, Country> countryObjects = gameData.gameMap.getAllCountries();
 
 		for (Player player : gameData.getPlayers()) {
 
+			// Gets all countries from game map.
 			Boolean firstTime = true;
 			HashSet<String> countriesPerPlayer = gameData.gameMap.getConqueredCountriesPerPlayer(player.getPlayerID());
-
-			String[] countriesPerPlayerArray = Arrays.copyOf(countriesPerPlayer.toArray(), countriesPerPlayer.size(),
-					String[].class);
-			Integer numberOfArmiesAvailablePerPlayer = startUp.initialArmyCalculation(gameData.getNoOfPlayers());
-
+			String[] countriesPerPlayerArray = Arrays.copyOf(countriesPerPlayer.toArray(), countriesPerPlayer.size(),String[].class);
+			numberOfArmiesAvailablePerPlayer = startUp.initialArmyCalculation(gameData.getNoOfPlayers());
+			
+			// Displays the number of armies per player.
 			System.out.println(" ");
-			System.out.println("Number of Army per player: " + numberOfArmiesAvailablePerPlayer);
+			System.out.println("Number of Armies per player: " + numberOfArmiesAvailablePerPlayer);
 
 			while (numberOfArmiesAvailablePerPlayer > 0) {
-
+				
+				// Displays the lists of countries owned by a player.
 				System.out.println("Player: " + player.getPlayerName() + " owns the following countries: ");
 
+				// At the beggining every country has at least one army.
 				for (int i = 0; i < countriesPerPlayerArray.length; i++) {
+
 					String countryName = countriesPerPlayerArray[i];
 
 					if (firstTime) {
 						countryObjects.get(countryName).addArmy(1);
 						numberOfArmiesAvailablePerPlayer--;
 					}
-					System.out.println("\t " + i + ": " + countryName + " has "
-							+ countryObjects.get(countryName).getCountryArmyCount() + " armies placed.");
+
+					System.out.println("\t " + i + ": " + countryName + " has " + countryObjects.get(countryName).getCountryArmyCount() + " armies placed.");
 				}
 
 				firstTime = false;
-				System.out.println("You have " + numberOfArmiesAvailablePerPlayer + " armies left.");
+				
+				String chosedCountryByUserStr = "";
+				
+				// The user chooses a country and place the army
+				do {
+					System.out.println("\nYou have " + numberOfArmiesAvailablePerPlayer + " armies left.");
+					System.out.println("Please pick the number associated with the country in order to place your armies: ");
 
-				System.out.println("Please pick the number associated with the country in order to place your armies: ");
+					chosedCountryByUserStr = input.nextLine();
 
-				//TODO : ERROR check
-				int chosedCountryByUser = input.nextInt();
+				} while (isNaN(chosedCountryByUserStr) 
+						|| Integer.parseInt(chosedCountryByUserStr) > countriesPerPlayerArray.length 
+						|| Integer.parseInt(chosedCountryByUserStr) < 0);
+				
+				int chosedCountryByUser = Integer.parseInt(chosedCountryByUserStr);
 				String countryName = countriesPerPlayerArray[chosedCountryByUser];
+				
+				String numberOfArmiesByUserStr = "";
+				
+				// The user chooses how many army wants to put in a choosed country.
+				do {
 
-				System.out.println("Player: " + player.getPlayerName() + " How many armies do you want to place in "
-						+ countryName + "?");
+					System.out.println("Player: " + player.getPlayerName() + " How many armies do you want to place in " + countryName + "?");
 
-				//TODO : ERROR check
-				int numberOfArmiesByUser = input.nextInt();
+					numberOfArmiesByUserStr = input.nextLine();
+
+				} while (isNaN(numberOfArmiesByUserStr)
+						|| Integer.parseInt(numberOfArmiesByUserStr) > numberOfArmiesAvailablePerPlayer
+						|| Integer.parseInt(numberOfArmiesByUserStr) < 0);
+				
+				
+				int numberOfArmiesByUser = Integer.parseInt(numberOfArmiesByUserStr);
 
 				input.close();
-
+				
+				// Adds the army to the country.
 				Country selectedCountry = countryObjects.get(countryName);
 				selectedCountry.addArmy(numberOfArmiesByUser);
 				numberOfArmiesAvailablePerPlayer = numberOfArmiesAvailablePerPlayer - numberOfArmiesByUser;
@@ -150,24 +189,31 @@ public class RiskGameDriver {
 		}
 
 	}
-
+	
+	/**
+     * Calls the start up generate round robin helper to generate the order to play. Then displays the order of play.
+     */
 	private void initiateRoundRobin() {
 		System.out.println(" ");
 		System.out.println("The following list has the order of the players in round robin fashion: ");
+		System.out.println("The results are based on the higher number of dice a player roled ");
 		ArrayList<Player> results = startUp.generateRoundRobin();
 		for(int i = 0; i < results.size(); i++) {
 			System.out.println((i+1) + " " + results.get(i).getPlayerName());
 		}
 	}
-
-	private void initiateRoundRobinBasedGamePlay() {
-		// TODO Auto-generated method stub
+	
+	/**
+     * Calls the assign countries to players from the controller.
+     */
+	private void ramdomAssignationOfCountries() {
+		// Country Assignment begins
+		startUp.assignCountriesToPlayers();
 	}
-
-	private void startTurn() {
-		// TODO Auto-generated method stub
-	}
-
+	
+	/**
+     * Helper method to test if a given strin can be converted to a int.
+     */
 	private boolean isNaN(final String string) {
 		try {
 			Integer.parseInt(string);
@@ -177,5 +223,14 @@ public class RiskGameDriver {
 		}
 		return false;
 	}
+	
+	private void initiateRoundRobinBasedGamePlay() {
+		// TODO Auto-generated method stub
+	}
+
+	private void startTurn() {
+		// TODO Auto-generated method stub
+	}
+	
 
 }
