a5e0e5140160f757cf63f1115f2d1d42c4d9a7be
karan_dhingra <karandhingra534@gmail.com>
2019-03-04 21:46:04 -0500

    user experience changes

--- src/com/main/RiskGameApplication.java
+++ src/com/main/RiskGameApplication.java
@@ -5,7 +5,9 @@ import com.java.view.RiskGameDriver;
 public class RiskGameApplication {
 
 	public static void main(String[] args) {
+		System.out.println("\n################ Game Begins ################\n");
 		RiskGameDriver driver = new RiskGameDriver();
 		driver.startGame();
+		System.out.println("\n################ Game Ends ################\n");
     }
 }
